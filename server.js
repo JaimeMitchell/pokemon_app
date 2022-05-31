6f5464bc5f7dc14154f5cd1ece2b124f91873fdc
Jaime Mitchell <JaimeMitchell100@gmail.com>
2022-05-31 10:27:46 -0400

    Connected my database, can see json in the browser

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
-
+// Set up your 'database' so that it can be exported to your server.js and then be required by your server.js
+const data = require('./models/pokemon')
+// Set this 'database' to a variable called pokemon in your server.js file
+const pokemon = data
 //set up root route and send response
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
     res.send('Welcome to the Pokemon App!')
+})
+// Create a get route /pokemon that will res.send(pokemon), which will display your pokemon data as json in the browser
+app.get('/pokemon', (req, res) => {
+    res.send(pokemon)
 })
 
 //set up port
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`PORT ${PORT} is listening`)
 })
